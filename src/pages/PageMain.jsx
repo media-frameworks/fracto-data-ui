c295addbb1cea59c834a3a6dd1860a81bd8d9cf0
EC2 Default User <mikehallvideo@gmail.com>
2023-02-16 17:10:24 +0000

    using AppPageMain now

--- src/pages/PageMain.jsx
+++ src/pages/PageMain.jsx
@@ -1,13 +1,38 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import styled from "styled-components";
+// import styled from "styled-components";
+//
+// import {CoolStyles} from 'common/ui/CoolImports';
+
+import AppPageMain from 'common/app/AppPageMain';
 
 export class PageMain extends Component {
 
-   static propTypes = {}
+   static propTypes = {
+      app_name: PropTypes.string.isRequired,
+   }
+
+   state = {
+      left_width: 0,
+      right_width: 0
+   };
+
+   on_resize = (left_width, right_width) => {
+      this.setState({
+         left_width: left_width,
+         right_width: right_width
+      })
+   }
 
    render() {
-      return "PageMain fracto-data"
+      const {left_width, right_width} = this.state;
+      const {app_name} = this.props;
+      return <AppPageMain
+         app_name={app_name}
+         on_resize={(left_width, right_width) => this.on_resize(left_width, right_width)}
+         content_left={["content left: ", left_width]}
+         content_right={["content right: ", right_width]}
+      />
    }
 }
 
